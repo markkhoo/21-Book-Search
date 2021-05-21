fba72095a4680b1080a8099b1241608c9b22eb1e
javimarashall <jamondragon@ucdavis.edu>
2021-05-21 13:21:58 -0700

    hero done

--- client/src/components/hero/hero.js
+++ client/src/components/hero/hero.js
@@ -1,1 +1,24 @@
-//
+import React from "react";
+
+function Alert(props) {
+  return (
+    <div>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Fluid jumbotron</h1>
+                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Alert;
+
+
+<div class="jumbotron jumbotron-fluid">
+<div class="container">
+<h1 class="display-4">Fluid jumbotron</h1>
+<p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+</div>
+</div>
