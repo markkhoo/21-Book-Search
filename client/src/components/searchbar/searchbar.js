4e7800953f1115432f99007ccd56c7b508aa5563
javimarashall <jamondragon@ucdavis.edu>
2021-05-21 13:16:57 -0700

    jumbotron re-made

--- client/src/components/searchbar/searchbar.js
+++ client/src/components/searchbar/searchbar.js
@@ -1,1 +1,16 @@
-//
+import React from "react";
+
+function search() {
+    const [search, setSearch] = useState();
+  
+    return (
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                </div>
+        </div>
+    );
+  }
+  
+  export default Count;
