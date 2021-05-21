29f4dbe9ca05f93f5fe1e5217a47622d4e504eff
javimarashall <jamondragon@ucdavis.edu>
2021-05-21 13:33:01 -0700

    container done

--- client/src/components/cardComponent/cardContainer.js
+++ client/src/components/cardComponent/cardContainer.js
@@ -1,1 +1,15 @@
-//
+import React from "react";
+
+function cardContainer(props) {
+  return (
+    <div>
+       <div className="container" onClick={() => this.props.handler(this.props.id)}>
+                    <div className="img-container">
+                        <img alt="memory game" src={this.props.image} />
+                    </div>
+                </div>
+    </div>
+  );
+}
+
+export default cardContainer;
