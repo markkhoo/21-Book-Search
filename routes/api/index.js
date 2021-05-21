f0e506bbb4a32af7223bf0335a43b71471c7c1c1
markeezy <markkhoo95@gmail.com>
2021-05-21 13:33:50 -0700

    server side routes WIP

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,7 @@
-//
+const router = require("express").Router();
+const booksRoutes = require("./posts");
+
+// Post routes
+router.use("/books", booksRoutes);
+
+module.exports = router;
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,1 +1,21 @@
-//
+const router = require("express").Router();
+const postsController = require("../../controllers/postsController");
+
+// Matches with "/api/books"
+router
+    .route("/")
+    .get(postsController.findAll);
+
+// Matches with "/api/books"
+router
+    .route("/")
+    .post(postsController.create);
+
+// Matches with "/api/books/:id"
+router
+    .route("/:id")
+    .get(postsController.findById)
+    .put(postsController.update)
+    .delete(postsController.remove);
+
+module.exports = router;
