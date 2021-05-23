c63ee9e13653dc855842349c787f1a886dde0767
javimarashall <jamondragon@ucdavis.edu>
2021-05-23 08:31:37 -0700

    axios fetch

--- client/src/components/cardComponent/cardContainer.js
+++ client/src/components/cardComponent/cardContainer.js
@@ -5,6 +5,7 @@ function CardContainer(props) {
   return (
     <div>
       <div className="container">
+        
         <div class="card mb-3" style="max-width: 540px;">
           <div class="row no-gutters">
             <div class="col-md-4">
--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -3,21 +3,42 @@ import Hero from "../components/hero"
 
 import Searchbar from "../components/searchbar";
 import CardContainer from "../cardContainer";
+import axios from "axios"
 function Search() {
-    const [search, setSearch] = useState("");
-    onchange()
-    submit()
+  const [search, setSearch] = useState("");
+  const [input, setInput] = useState("");
+  const [books,setBooks] = useState([]);
+
+  const handleSetInput = (event) => {
+    setInput(event.target.value)
+  }
+
+  const handleBookSearch = () => {
+    //get  book data from server route
+    axios.post(`/api/books/google/${input}`).then((res) => {
+      console.log("*******",res)
+    });
+  }
+
+
 
   return (
-    <>
-        
-       
-        
-      
 
-    </>    
-        
-    
+    <div class="input-group mb-3">
+      <input 
+        type="text" 
+        class="form-control"
+        placeholder="Recipient's username" 
+        aria-label="Recipient's username" 
+        aria-describedby="button-addon2"
+        onChange={handleSetInput} 
+      />
+      <div class="input-group-append">
+          <button onClick={handleBookSearch} class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+      </div>
+    </div>
+
+
   );
 }
 
