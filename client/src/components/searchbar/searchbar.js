fd2eb432886b8f182be1d2920a3d1f2d7186b12d
Manuel Villasenor <manuelvillasenor@2@gamil.com>
2021-05-25 15:46:35 -0700

    push

--- client/src/components/searchbar/searchbar.js
+++ client/src/components/searchbar/searchbar.js
@@ -1,1 +1,63 @@
-//
+import API from "../../utils/API";
+import React, { useEffect, useState } from "react";
+import { getBooks } from "../utils/actions";
+
+
+
+const BooksRender = props => {
+    const [book, setBook] = useState()
+
+    // Load all books and store them with setBooks
+    useEffect(() => {
+        API.getBooks(bookName)
+            .then(res => setBook())
+            .catch(err => console.log(err));
+    }, [])
+
+    const addBookToDB = () => {
+        setBook({
+            type: ADD_FAVORITE,
+            post: book.currentPost
+          });
+    }
+
+
+    function googleBooks() {
+
+        API.getBooks(bookName)
+    }
+
+
+
+
+    return (
+        
+        
+        <div>
+            <div className="card" style="width: 18rem;">
+                <img className="card-img-top" src="..." alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">{book.title}</h5>
+                    <p className="card-text">{book.description}</p>
+                </div>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"></li>
+                    <li className="list-group-item">{book.authors}</li>
+                    <li className="list-group-item">Vestibulum at eros</li>
+                    
+                </ul>
+                <button className="btn btn-danger" onClick={addBookToDB}>
+                    addBookToDB
+                </button>
+
+                <div className="card-body">
+                    <a href="#" className="card-link">Card link</a>
+                    <a href="#" className="card-link">Another link</a>
+                </div>
+            </div>
+        </div>
+    )
+
+
+
+}
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,1 +1,9 @@
-//
+import axios from "axios";
+
+export default {
+  // Gets all books
+  getBooks: function(bookName) {
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q="+bookName);
+  },
+
+};
